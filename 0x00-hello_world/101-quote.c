0b3dba32ee598435696fb18d77adc513c962eb33
Kumi <kumimoses@outlook.com>
2022-09-08 22:48:40 -0700

    This project teach us how to create a simple hello world C program!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - prints the standrad error sandwiched around the  standard outpu/t
- *
- * Return: returns 1
- */
+   * main - Entry point
+    *
+     * Return: Always 1 (Success)
+      */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+		write(2,
+				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
 }
 
